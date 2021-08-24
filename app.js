cdb120ee25a2a0378e2a0646c027dfa3302a3da4
Sumit Saurabh <sumitsaurabh927@gmail.com>
2021-08-24 12:04:43 +0530

    getting date 'value' in js

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-console.log("working fine");
+var btn=document.querySelector("#btn")
+var date=document.querySelector("#date")
 
+function clickHandler(){
+    console.log(date.value);
+
+}
+
+btn.addEventListener("click",clickHandler)
